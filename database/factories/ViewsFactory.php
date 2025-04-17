e0e453f2d0788dce1b1eea85a7b004ef2780a0f9
santiago305 <minecratf633@gmail.com>
2025-04-16 22:50:31 -0500

    Restore ViewsFactory: reintroduce ViewsFactory class for generating view data with device type and browser information.

--- database/factories/ViewsFactory.php
+++ database/factories/ViewsFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Views>
  */
-class ViewFactory extends Factory
+class ViewsFactory extends Factory
 {
     /**
      * Define the model's default state.
