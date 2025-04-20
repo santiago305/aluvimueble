7cc8ee10fac406ca6404f7e3403b2f271de68155
santiago305 <minecratf633@gmail.com>
2025-04-20 00:22:54 -0500

    fix(app): comment out ForceHttps middleware in application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-            ForceHttps::class,
+            // ForceHttps::class,
             // LogPageView::class,
         ]);
 
