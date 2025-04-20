64245c87690585bd40f36112133d475f7306fdee
santiago305 <minecratf633@gmail.com>
2025-04-20 00:19:14 -0500

    feat(middleware): add ForceHttps middleware to enforce HTTPS connections

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceHttps;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\LogPageView;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            ForceHttps::class,
             // LogPageView::class,
         ]);
 
--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,5 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
+    base: 'https://aluvimueble-production.up.railway.app/',
 });
